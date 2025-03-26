4b039471000443ff17c35067c4a194c33b22bd1e
Andi <andi.uhle@tktk.ee>
2025-03-26 17:36:02 +0200

    StringsandArrays

---
+++ Week6StringsandArrays/Program.cs
@@ -0,0 +1,19 @@
+﻿string hello = "hello, world! Here I am!";
+
+int stringLength = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+
+int wordCounter = 1;
+
+for(int i = 0; i < hello.Length; i++)
+{
+    if (hello[i] == ' ')
+    {
+        wordCounter++;
+    }
+}
+
+Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
